858ab43f92fafbaf3461122d87081de42274f637
yPinn <david010556@gmail.com>
2026-03-31 12:33:58 +0800

    fix(auth): align basePath with AUTH_URL for /pikora subpath deployment

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -13,6 +13,18 @@ import { authConfig } from './config';
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
   adapter: PrismaAdapter(prisma),
-  basePath: '/pikora/api/auth',
+  basePath: '/api/auth',
+  // 加上這個可以確保獨立性
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token.pikora`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/pikora', // 讓瀏覽器只在 /pikora 下發送這個 Cookie
+        secure: true,
+      },
+    },
+  },
   debug: process.env.NODE_ENV === 'development',
 });
